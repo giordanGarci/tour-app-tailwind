ad537027a7001d731b7d641dab61ac3d1ffc2de7
Giordan Garcia <giordangarcia12@gmail.com>
2025-11-15 17:16:52 -0300

    validation form

--- src/app/categorias/categoria/categoria.component.ts
+++ src/app/categorias/categoria/categoria.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-categoria',
@@ -7,5 +8,22 @@ import { Component } from '@angular/core';
   styleUrl: './categoria.component.scss'
 })
 export class CategoriaComponent {
+  camposForm: FormGroup;
 
+  constructor() {
+    this.camposForm = new FormGroup({
+      nome: new FormControl('', Validators.required),
+      descricao: new FormControl('', Validators.required)
+    });
+  }
+
+  onSave() {
+    console.log(this.camposForm.value);
+    console.log(this.camposForm.valid);
+  }
+
+  isFieldInvalid(campo: string) : boolean {
+    const field = this.camposForm.get(campo);
+    return (field?.invalid && field?.touched && field?.errors?.['required']) || false;
+  }
 }
