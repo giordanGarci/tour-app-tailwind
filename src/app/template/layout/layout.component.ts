e322a23794136cc5494c9a8ab9f60d585a960420
Giordan Garcia <giordangarcia12@gmail.com>
2025-11-27 21:58:58 -0300

    Capturing route events to set the title and subtitle of each page

--- src/app/template/layout/layout.component.ts
+++ src/app/template/layout/layout.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LayoutProps } from './layoutprops';
+import { ActivatedRoute, Router } from '@angular/router';
+import { filter, map } from 'rxjs';
 
 @Component({
   selector: 'app-layout',
@@ -6,6 +9,32 @@ import { Component } from '@angular/core';
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss'
 })
-export class LayoutComponent {
+export class LayoutComponent implements OnInit {
+  props: LayoutProps = {
+    title: 'Default Title',
+    subtitle: 'Default Subtitle'
+  };
 
+  constructor(
+    private router: Router,
+    private activateRoute: ActivatedRoute
+  ) { }
+
+  ngOnInit(): void {
+    this.router.events
+      .pipe(
+        filter(() => this.activateRoute.firstChild !== null),
+        map(() => this.getPropsLayout())
+      ).subscribe(props => {
+        this.props = props;
+      });
+  }
+
+  getPropsLayout(): LayoutProps {
+    let childRoute = this.activateRoute.firstChild;
+    while (childRoute?.firstChild) {
+      childRoute = childRoute.firstChild;
+    }
+    return childRoute?.snapshot.data as LayoutProps || this.props;
+  }
 }
--- src/app/template/template-routing.module.ts
+++ src/app/template/template-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LayoutComponent } from './layout/layout.component';
+
 const routes: Routes = [
   {
     path: "",
@@ -9,17 +10,20 @@ const routes: Routes = [
       {
         path: "categorias",
         loadChildren: () => import('../categorias/categories.module').then(m => m.CategoriasModule),
-        pathMatch: 'full'
+        pathMatch: 'full',
+        data: { title: 'Categorias', subtitle: 'Realize o cadastro de categorias' }
       },
       {
         path: "lugares",
         loadChildren: () => import('../lugares/places.module').then(m => m.PlacesModule),
-        pathMatch: 'full'
+        pathMatch: 'full',
+        data: { title: 'Lugares', subtitle: 'Realize o cadastro de lugares' }
       },
       {
         path: "galeria",
         loadChildren: () => import('../gallery/gallery.module').then(m => m.GalleryModule),
-        pathMatch: 'full'
+        pathMatch: 'full',
+        data: { title: 'Lista de lugares', subtitle: 'Descubra os melhores lugares para visitar etc' }
       }
     ]
   }
